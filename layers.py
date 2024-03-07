1261e1ccd43927b97ffc1af35f1ea4bc7997dc9b
Namreh <herman@neple.no>
2024-03-07 09:58:33 +0100

    La inn oppdatering av parametere

--- layers.py
+++ layers.py
@@ -124,6 +124,12 @@ class Attention(Layer):
         g_ov = self.Wv.backward(self.Wo.backward(self.grad))
         g_s = self.softmax.backward(np.einsum('bij,bik->bjk', self.z, g_ov))
 
+        #oppdaterer gradient for parameterene ifølge ligninger 22-25
+        self.Wo.params['w']['d'] = np.einsum('ij,bjk,bkl,bml->bim', self.Wv.params['w']['w'], self.z, self.A, self.grad)
+        self.Wv.params['w']['d'] = np.einsum('ij,bjk,blk,bml->bim', self.Wo.params['w']['w'], self.grad, self.A, self.z)
+        self.params['wk']['d'] = np.einsum('ij,bjk,bkl,bml->bim', self.Wq,self.z,g_s,self.z)
+        self.params['wq']['d'] = np.einsum('ij,bjk,blk,bml->bim', self.Wk,self.z,g_s,self.z)
+
 
         return self.grad + np.einsum('bij,bkj->bik', g_ov, self.A) + np.einsum('ij,ik,bkl,blm->bjm', self.Wk, self.Wq, self.z, g_s) + np.einsum('ij,ik,bkl,bml->bjm', self.Wq, self.Wk, self.z, g_s)
     
